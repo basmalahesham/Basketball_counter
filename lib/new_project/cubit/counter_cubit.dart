d7b38892d9d7f1ce3e73cfe45a42ae7cdae28406
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-23 04:31:27 +0300

    create counter states create counter cubit

---
+++ lib/new_project/cubit/counter_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:basketball_counter_app/new_project/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState>{
+  CounterCubit(super.initialState);
+
+}
